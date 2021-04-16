fb34d7c9816046e3e67b443e3644a6f747874b90
Jan Krzysztof Sosulski <jan.sosulski.stud@pw.edu.pl>
2021-04-16 14:57:50 +0200

    resolving merged problems v1

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,8 @@
 public class Main {
     //adding main
     public static void main(String[] args) {
-        System.out.println("Hello!");
+        System.out.println("Hey!");
+        //test
+        System.out.println("Test");
     }
 }
