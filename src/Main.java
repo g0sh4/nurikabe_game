a45c0add91a8c45f067bbc17e4fabe1a46991b82
Jakub <jakubkoz106@gmail.com>
2021-04-20 22:47:01 +0200

    sprawdzam czy dziala ~kuba

--- src/Main.java
+++ src/Main.java
@@ -6,5 +6,6 @@ public class Main {
     public static void main(String[] args) {
         GuiMenu gm1 = new GuiMenu();
         System.out.println("elo");
+        System.out.println("dodal ~kuba");
     }
 }
