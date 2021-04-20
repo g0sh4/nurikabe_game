470214343479e20b2b9d0443a67bc78d07407172
Małgorzata Olech <01159108@pw.edu.pl>
2021-04-20 20:59:50 +0200

    adding "cokolwiek"

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,7 @@ import Gui.GuiMenu;
 public class Main {
     //adding main
     public static void main(String[] args) {
+        System.out.println("cokolwiek");
         GuiMenu gm1 = new GuiMenu();
     }
 }
