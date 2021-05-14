4ec1ee0e847b17c4d7480eb9007c1ab1e462a3f9
Małgorzata Olech <01159108@pw.edu.pl>
2021-05-14 17:51:26 +0200

    AboutAuthors + ikonki

---
+++ src/interaction/AboutAuthors.java
@@ -0,0 +1,23 @@
+package interaction;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class AboutAuthors {
+    public AboutAuthors(){
+        JFrame frame = new JFrame("Zasady gry Nurikabe");
+        frame.add(new JLabel( "Zasady gry: " ));
+        JOptionPane.showMessageDialog(frame,
+                """
+                        Jan Sosulski
+                        Jakub Kozłowski
+                        Małgorzata Olech
+                        """,
+                "Autorzy gierki",
+                JOptionPane.INFORMATION_MESSAGE, new ImageIcon( "brain.png" ));
+
+    }
+    public static void main(String[] args) {
+        new AboutAuthors();
+    }
+}
