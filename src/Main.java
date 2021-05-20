48f60ebf7e331737d09e9e7673ee50527f320453
Jan Krzysztof Sosulski <jan.sosulski.stud@pw.edu.pl>
2021-05-20 11:40:58 +0200

    ustawienia.png change package, changes in settings, changes in main

--- src/Main.java
+++ src/Main.java
@@ -10,8 +10,5 @@ public class Main {
     //adding main
     public static void main(String[] args) throws IOException, CsvException {
         new StartGame();
-        OpenCsvData openCsvData = new OpenCsvData("src/csv/boards.csv");
-        System.out.println(openCsvData.csvData.get(1)[2]);
-        System.out.println(openCsvData.getBoardNames());
     }
 }
